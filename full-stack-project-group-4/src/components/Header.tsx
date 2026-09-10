2194a75db4297a71e39d303408e75b4823fb871a
sboyer2 <sboyer2@academic.rrc.ca>
2026-09-10 17:30:59 -0500

    Added h1 tag for header

--- full-stack-project-group-4/src/components/Header.tsx
+++ full-stack-project-group-4/src/components/Header.tsx
@@ -1,6 +1,8 @@
 function Header () {
     return(
-        <header className="Header">Red River Rentals</header>
+        <header className="Header">
+            <h1>Red River Rentals</h1>
+            </header>
     )
 }
 export default Header
