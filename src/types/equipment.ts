065325a0642dbebf34d15c5e20c605f7dd12f8d6
Edraque Maglaque <emaglaque2@academic.rrc.ca>
2026-09-15 16:49:28 -0500

    Added a data interface for Equipments.

---
+++ src/types/equipment.ts
@@ -0,0 +1,11 @@
+export interface Equipment {
+    id: number;
+    name: string;
+    category: string;
+    description: string;
+    quantity: number;
+    availableQuantity: number;
+    condition: string;
+    location: string;
+    picture: string;
+}
