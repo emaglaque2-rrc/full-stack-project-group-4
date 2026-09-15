62513d4de65456296c343ba25dc84300ac7f9bcb
Edraque Maglaque <emaglaque2@academic.rrc.ca>
2026-09-15 16:50:09 -0500

    Added comments for viewability and reasoning about the Equipment interface.

--- src/types/equipment.ts
+++ src/types/equipment.ts
@@ -1,3 +1,10 @@
+// As for id property, I didn't name it equipmentId because when another data structure
+// needs to reference this equipment, it will be referenced as equipment.id, not equipment.equipmentId. 
+// So I think it's better to just name it id.
+//
+// Then for quantity and availableQuantity, I think it's better to have both instead of a 'status' property, because
+// we won't know the total number of equipment items, how many items are currently available for use, 
+// and we won't know how many items are currently in use.
 export interface Equipment {
     id: number;
     name: string;
