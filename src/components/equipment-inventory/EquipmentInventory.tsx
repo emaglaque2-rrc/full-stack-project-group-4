fd789379f35e33807d48bc1bfdc00a58075e4256
Edraque Maglaque <emaglaque2@academic.rrc.ca>
2026-09-15 17:49:59 -0500

    Initialized a list for my equipment, it'll be an array of my equipment objects.

--- src/components/equipment-inventory/EquipmentInventory.tsx
+++ src/components/equipment-inventory/EquipmentInventory.tsx
@@ -2,6 +2,8 @@ import type { Equipment } from '../../types/equipment';
 import equipmentData from '../../data/equipment.json';
 
 function EquipmentInventory() {
+    const equipmentList: Equipment[] = equipmentData;
+    
     return(
         <section className="equipment-inventory">
             {/** placeholder for now */}
