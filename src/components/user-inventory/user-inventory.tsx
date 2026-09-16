952bc25630ff1cfc5ad9fa6ee8dc25fa199fdeae
Zalak <zpunjani@academic.rrc.ca>
2026-09-16 01:19:32 -0500

    Revert "Zalak user inventory tsx feature"

--- src/components/user-inventory/user-inventory.tsx
+++ src/components/user-inventory/user-inventory.tsx
@@ -20,40 +20,9 @@ export const userData: User[] = users;
 
 function UserInventory() {
     return (
-        <section className="user-inventory">
+        <main>
             <h2> User Information</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>User ID</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Equipment ID</th>
-                        <th>Date Rented</th>
-                        <th>Date Returned</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {userData.map((user) =>
-                        user.rentals.map((rental) =>
-                            <tr key={`${user.userId}-${rental.equipmentId}`}>
-                                <td>{user.userId}</td>
-                                <td>{user.firstName}</td>
-                                <td>{user.lastName}</td>
-                                <td>{user.userEmail}</td>
-                                <td>{rental.equipmentId}</td>
-                                <td>{rental.dateRented}</td>
-                                <td>{rental.dateReturned}</td>
-                                <td>{rental.status}</td>
-                            </tr>
-                        ))}
-                </tbody>
-            </table>
-
-        </section>
+        </main>
     )
 }
 
