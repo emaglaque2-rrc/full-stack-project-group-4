f2d58d132ea312a06e4ea113fa9da8a1141ba4aa
sboyer2 <sboyer2@academic.rrc.ca>
2026-09-15 23:17:41 -0500

    Basic review HTML generation done

---
+++ src/components/reviews/reviews.tsx
@@ -0,0 +1,24 @@
+import type { Review } from "../../types/review";
+import reviews from "../../data/reviews.json"
+
+function renderReviews() {
+    return (
+        <section className="review-display">
+            <h2>Product Reviews</h2>
+
+            {reviews.map((review) => (
+                <article className="review">
+                    <h3>{review.title} | {review.rating}/10</h3>
+                    <p>Author: {review.author}</p>
+                    <p>Product: {review.product}</p>
+                    <p>{review.body}</p>
+                    <p>Posted on {review.datePosted}</p>
+                
+                </article>
+            ))}
+         
+        </section>
+    )
+}
+
+export default renderReviews
