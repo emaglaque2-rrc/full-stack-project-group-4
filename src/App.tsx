ca10f85bfd883b6fd132b8702e6cc70d707aefc2
Zalak Punjani <zpunjani@academic.rrc.ca>
2026-09-15 23:34:53 -0500

    Added the export for the userInventory and added the function to the app.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import UserInventory from './components/user-inventory/user-inventory'
 
 function App() {
 
   return (
     <>
         <Header />
+        <UserInventory />
         <Footer />
     </>
   )
--- src/components/user-inventory/user-inventory.tsx
+++ src/components/user-inventory/user-inventory.tsx
@@ -58,3 +58,5 @@ function UserInventory() {
         </section>
     )
 }
+
+export default UserInventory
