1bf0d5935f2f9c72f6245aa47671bec7103940ce
Edraque Maglaque <emaglaque2@academic.rrc.ca>
2026-09-15 20:07:09 -0500

    Finished the EquipmentItem component to extract the details from the equipment.

--- src/components/equipment-item/EquipmentItem.tsx
+++ src/components/equipment-item/EquipmentItem.tsx
@@ -3,3 +3,17 @@ import type { Equipment } from '../../types/equipment';
 interface EquipmentItemProps {
     equipment: Equipment;
 }
+
+function EquipmentItem({ equipment }: EquipmentItemProps) {
+    return (
+        <article className="equipment-item">
+            <h3>{equipment.name}</h3>
+            <p>{equipment.description}</p>
+            <p>Quantity: {equipment.quantity}</p>
+            <p>Available: {equipment.availableQuantity}</p>
+            <p>Condition: {equipment.condition}</p>
+            <p>Location: {equipment.location}</p>
+            <p>Category: {equipment.category}</p>
+        </article>
+    )
+}
