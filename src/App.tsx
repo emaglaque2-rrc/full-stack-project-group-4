41c20340b9953db07ee5812914aabeac73060663
Zalak Punjani <zpunjani@academic.rrc.ca>
2026-09-16 00:09:52 -0500

    By mistake did a pull and now i am fixing the improts in the app.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import EquipmentInventory from './components/equipment-inventory/EquipmentInventory'
+import UserInventory from './components/user-inventory/user-inventory'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
 
       <main>
         <EquipmentInventory />
+        <UserInventory />
       </main>
       
       <Footer />
