3439d2cd461b528b5591fd6c70d5ff1bf65e65c3
Edraque Maglaque <emaglaque2@academic.rrc.ca>
2026-09-15 21:44:35 -0500

    Integrated my EquipmentInventory component into App.tsx and wrapped it in a <main> tag.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
-import Header from './components/Footer/Footer'
+import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import EquipmentInventory from './components/equipment-inventory/EquipmentInventory'
 
 function App() {
 
   return (
     <>
+
       <Header />
+
+      <main>
+        <EquipmentInventory />
+      </main>
+      
       <Footer />
+
     </>
   )
 }
