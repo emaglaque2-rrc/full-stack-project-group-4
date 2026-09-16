4b3a5a03567355a0f89c3ff10de01fed81522f07
sboyer2 <sboyer2@academic.rrc.ca>
2026-09-15 22:20:59 -0500

    Made review interface & added placeholder review json

---
+++ src/types/review.ts
@@ -0,0 +1,11 @@
+export interface Review {
+    id: number;
+    title: string;
+    author: string;
+    body: string;
+    rating: number;
+    datePosted: Date;
+    dateRented: Date;
+    dateUpdated: Date;
+}    
+
